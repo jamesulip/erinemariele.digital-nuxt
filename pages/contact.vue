e5909f66a5405de88c300e8c3bdd556689da8f13
jamesulip <ulip.james@gmail.com>
2023-01-24 16:30:43 +0800

    added

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,25 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: foo
+  <div class="flex w-full">
+    <div class="m-auto flex  max-w-2xl gap-10">
+      <div class="bg-black flex">
+        <img src="/img/about-img.jpg" class="w-full m-auto"/>
+      </div>
+      <div class="flex flex-col">
+        <span class=" font-['Josefin_Sans'] font-normal text-[15px]">Hello, Im</span>
+        <span class=" font-['Josefin_Sans'] font-bold text-[40px]">Erine Mariele</span>
+        <span class=" font-['Josefin_Sans'] font-light text-[40px]">Atordido</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident culpa error nostrum doloremque molestias quae voluptates eveniet libero cumque nihil! Accusamus animi impedit dolorum, culpa officiis sunt temporibus debitis ratione.
+        </p>
+        <br>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, voluptatem. Libero cum facilis omnis. Necessitatibus eligendi quod accusantium iusto alias? Laudantium, illo. Reprehenderit impedit asperiores debitis molestias consequuntur ab quasi?
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
