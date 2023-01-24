64b109633c78c8e1857cbadb20f7e1e92bf6c567
jamesulip <ulip.james@gmail.com>
2023-01-24 16:38:01 +0800

    code fix, added page for contact and about

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,25 +2,45 @@
 
 <template>
   <div class="flex w-full">
-    <div class="m-auto flex  max-w-2xl gap-10">
-      <div class="bg-black flex">
-        <img src="/img/about-img.jpg" class="w-full m-auto"/>
-      </div>
-      <div class="flex flex-col">
-        <span class=" font-['Josefin_Sans'] font-normal text-[15px]">Hello, Im</span>
-        <span class=" font-['Josefin_Sans'] font-bold text-[40px]">Erine Mariele</span>
-        <span class=" font-['Josefin_Sans'] font-light text-[40px]">Atordido</span>
+    <div class="m-auto grid grid-cols-2  max-w-2xl gap-10">
+      <!-- create a form for name email and message -->
+     <div>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident culpa error nostrum doloremque molestias quae voluptates eveniet libero cumque nihil! Accusamus animi impedit dolorum, culpa officiis sunt temporibus debitis ratione.
-        </p>
-        <br>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, voluptatem. Libero cum facilis omnis. Necessitatibus eligendi quod accusantium iusto alias? Laudantium, illo. Reprehenderit impedit asperiores debitis molestias consequuntur ab quasi?
-        </p>
-      </div>
+     </div>
+     <form class="flex flex-col gap-5 font-['Josefin_Sans']">
+        <span class=" font-['Josefin_Sans'] font-normal text-[30px]">Get in Touch</span>
+        <table class="w-[500px] ">
+          <tr>
+            <td>
+              Name
+            </td>
+            <td class="py-1.5">
+              <input type="text" name="name" id="name" class="w-full border-2 border-gray-200 p-2" />
+            </td>
+          </tr>
+          <tr >
+            <td>
+              Email
+            </td>
+            <td class="py-1.5">
+              <input type="email" name="email" id="email" class="w-full border-2 border-gray-200 p-2" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Message
+            </td>
+            <td class="py-1.5">
+              <textarea name="message" id="message" cols="30" rows="10"
+                class="w-full border-2 border-gray-200 p-2"></textarea>
+            </td>
+          </tr>
+        </table>
+      </form>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
